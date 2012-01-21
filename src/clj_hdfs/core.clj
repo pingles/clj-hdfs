7465c9f6c065e7a0270bd1569b7e31931e732fd1
Paul Ingles <paul@oobaloo.co.uk>
2012-01-21 15:26:02 +0000

    create a sequence file

--- src/clj_hdfs/core.clj
+++ src/clj_hdfs/core.clj
@@ -1,7 +1,8 @@
 (ns clj-hdfs.core
   (:import [org.apache.hadoop.conf Configuration]
            [org.apache.hadoop.mapred JobConf]
-           [org.apache.hadoop.fs Path]))
+           [org.apache.hadoop.fs Path]
+           [org.apache.hadoop.io SequenceFile]))
 
 (defn create-configuration
   [m]
@@ -9,12 +10,15 @@
     (doseq [[n v] m] (.set c n v))
     c))
 
-(defn create-job-conf
-  [m]
-  (JobConf. (configuration m)))
-
-
 (defn path
   "Creates a Path from the specified path."
   [str]
   (Path. str))
+
+(defn create-sequence-writer
+  [conf path key-class val-class]
+  (SequenceFile/createWriter (.getFileSystem path conf)
+                             conf
+                             path
+                             key-class
+                             val-class))
--- test/clj_hdfs/test/core.clj
+++ test/clj_hdfs/test/core.clj
@@ -1,4 +1,14 @@
 (ns clj-hdfs.test.core
+  (:import [java.io File]
+           [org.apache.hadoop.io LongWritable])
   (:use [clojure.test]
-        [clj-hdfs.core]))
+        [clj-hdfs.core :only (path create-sequence-writer create-configuration)] :reload))
 
+(def config (create-configuration {}))
+
+(deftest sequence-file-writing
+  (let [tmp-path "./tmp/test.seq"
+        key-class LongWritable
+        val-class LongWritable]
+    (with-open [writer (create-sequence-writer config (path tmp-path) key-class val-class)])
+    (is (true? (.exists (File. tmp-path))))))
